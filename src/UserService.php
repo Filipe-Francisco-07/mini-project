781d93c38a15ae071b4748dd4da3a6518345c14b
Filipe F <111152966+Filipe-Francisco-07@users.noreply.github.com>
2026-03-08 23:00:19 -0300

    DocGen update

---
+++ src/UserService.php
@@ -0,0 +1,45 @@
+
+<?php
+
+/**
+ * Classe responsável pela gestão de usuários, incluindo criação e busca de usuários por ID.
+ */
+
+class UserService
+{
+
+    /**
+     * Cria um novo usuário com um ID aleatório, nome e e-mail fornecidos.
+     * 
+     * @param mixed $name O nome do usuário a ser criado.
+     * @param mixed $email O e-mail do usuário a ser criado.
+     * @return mixed Um array contendo o ID, nome e e-mail do usuário.
+     */
+
+    public function createUser($name, $email)
+    {
+        return [
+            "id" => rand(1, 1000),
+            "name" => $name,
+            "email" => $email
+        ];
+    }
+    /**
+     * Retorna um array com informações de um usuário baseado no ID fornecido.
+     * 
+     * @param mixed $id O identificador do usuário a ser buscado.
+     * @return mixed Um array contendo o ID, nome e email do usuário.
+     */
+  
+    public function findUserById($id)
+    {
+        return [
+      
+            "id" => $id,
+            "name" => "Usuário Teste",
+            "email" => "teste@email.com"
+ 
+        ];
+    }
+
+}
